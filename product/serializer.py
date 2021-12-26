610d4c9e4e441f42f20a6c642db07bc3531c13c8
frolovrv <frolloff1@yandex.ru>
2021-12-26 15:17:36 +0300

    create ProductSerializer

---
+++ product/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializer
+
+from .models import Category, Product
+
+class ProductSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail",
+        )
