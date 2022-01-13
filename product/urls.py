a27d35c71acd3c5d88b063910257e51253492510
frolovrv <frolloff1@yandex.ru>
2022-01-13 23:40:49 +0500

    add search functionality

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
-from product import views
+from product.views import LatestProductsList, CategoryDetail, ProductDetail, search
 
 urlpatterns = [
-    path('latest-products/', views.LatestProductsList.as_view()),
-    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
-    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+    path('latest-products/', LatestProductsList.as_view()),
+    path('products/search/', search),
+    path('products/<slug:category_slug>/', CategoryDetail.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view()),
 ]
